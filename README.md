1ddc1548448be0268a2a73df0888056acf46ed43
sitaramshelke <sitaramshelke@gmail.com>
2018-11-22 14:55:30 +0530

    Add example for react-live with all dependencies

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Check independant branches for example code.
+This code uses [react-live](https://github.com/FormidableLabs/react-live), just the preview part. 
+It loads all the dependencies in the final bundle. Then at the run time just defines the scope and fetches the user's react code to be rendered.
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require("path");
+var webpack = require("webpack")
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      ReactTable: 'react-table'
+    })
+  ]
+};
